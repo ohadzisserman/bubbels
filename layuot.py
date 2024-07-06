bba434657f962056c17f28fa13d9ed5ec5b678ed
ohadzisserman <34860006+ohadzisserman@users.noreply.github.com>
2024-07-06 21:25:07 +0300

    Delete layuot.py

--- layuot.py
+++
@@ -1,9 +0,0 @@
-from dash import Dash, html
-def create_layout(app: Dash) -> html.Div:
-    return html.Div(
-        className="my appush",
-        children=[
-            html.H1(app.title),
-            html.Hr()
-        ]
-    )
